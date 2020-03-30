61a9b509e9edb20b98e41746be7d72ae42e32ed2
meayesha <30566794+meayesha@users.noreply.github.com>
2020-03-30 15:27:33 -0400

    Chat application named Gossip using firebase

---
+++ ChatGossip/app/src/main/java/edu/psu/aqp5794/chatgossip/Fragments/UsersFragment.java
@@ -0,0 +1,158 @@
+package edu.psu.aqp5794.chatgossip.Fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import edu.psu.aqp5794.chatgossip.Adapter.UserAdapter;
+import edu.psu.aqp5794.chatgossip.Model.User;
+import edu.psu.aqp5794.chatgossip.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static androidx.constraintlayout.widget.Constraints.TAG;
+
+
+public class UsersFragment extends Fragment {
+
+
+    private RecyclerView recyclerView;
+
+    private UserAdapter userAdapter;
+    private ArrayList<User> mUsers;
+
+    EditText search_users;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_users, container, false);
+
+        recyclerView = view.findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        mUsers = new ArrayList<>();
+
+        readUsers();
+
+//        search_users = view.findViewById(R.id.search_users);
+//        search_users.addTextChangedListener(new TextWatcher() {
+//            @Override
+//            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//
+//            }
+//
+//            @Override
+//            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//                searchUsers(charSequence.toString().toLowerCase());
+//            }
+
+//            @Override
+//            public void afterTextChanged(Editable editable) {
+//
+//            }
+//        });
+//
+        return view;
+   }
+
+//    private void searchUsers(String s) {
+//
+//        final FirebaseUser fuser = FirebaseAuth.getInstance().getCurrentUser();
+//        Query query = FirebaseDatabase.getInstance().getReference("Users").orderByChild("search")
+//                .startAt(s)
+//                .endAt(s+"\uf8ff");
+//
+//        query.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                mUsers.clear();
+//                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+//                    User user = snapshot.getValue(User.class);
+//
+//                    assert user != null;
+//                    assert fuser != null;
+//                    if (!user.getId().equals(fuser.getUid())){
+//                        mUsers.add(user);
+//                    }
+//                }
+//
+//                userAdapter = new UserAdapter(getContext(), mUsers, false);
+//                recyclerView.setAdapter(userAdapter);
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//            }
+//        });
+//
+//    }
+//
+    private void readUsers(){
+
+        final FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users");
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+              // if (search_users.getText().toString().equals("")) {
+                    mUsers.clear();
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        User user = snapshot.getValue(User.class);
+                       // Log.d(TAG, "onDataChange: " + dataSnapshot.getValue(User.class));
+                        //delete this part
+                        assert user !=null;
+                        assert firebaseUser !=null;
+
+                        //till here
+                        int c=0;
+                        if (!user.getId().equals(firebaseUser.getUid())) {
+                            mUsers.add(user);
+                            Log.d(TAG, "onDataChange: " + mUsers.get(c));
+                            c++;
+                        }
+
+                    }
+
+                    userAdapter = new UserAdapter(getContext(), mUsers,false);
+                    recyclerView.setAdapter(userAdapter);
+                }
+        //}
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+}
